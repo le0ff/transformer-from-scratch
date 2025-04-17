8c67fdf95f6ad1a8f0529983be930391c11409d4
benitomano <benito.manocchio@gmail.com>
2025-04-17 10:23:21 +0200

    add initial residual connection

---
+++ src/layers/residual.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+from numpy import ndarray
+
+from src.layers.base import BaseLayer
+from src.layers.dropout import Dropout
+from src.layers.normalization import LayerNorm
+
+
+class ResidualConnection(BaseLayer):
+    def __init__(self, normalized_shape: int, dropout_rate: float = 0.0):
+        """
+        Initializes ResidualConnection layer.
+
+        Parameters:
+            dropout_rate (float): Dropout rate for the layer.
+        """
+        super().__init__()
+        self.dropout = Dropout(dropout_rate)
+        self.layer_norm = LayerNorm(normalized_shape=normalized_shape)
+
+    def forward(self, x: ndarray, sublayer: Any):
+        """
+        Forward pass through the residual connection layer.
+
+        Parameters:
+            x (ndarray): Input data.
+            sublayer (Any): The sublayer to apply the residual connection to.
+
+        Returns:
+            ndarray: Output data after applying the residual connection.
+        """
+        return x + self.dropout(sublayer(self.layer_norm(x)))
